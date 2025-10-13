fa4b07aab8c33efd57e034f7289d3618dfb1a44f
Роман Козырев <Tyuiu.KozyrevRA@outlook.com>
2025-10-13 19:19:34 +0500

    Task1

---
+++ Tyuiu.KozyrevRA.Sprint5.Task1.V14.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KozyrevRA.Sprint5.Task1.V14.Lib;
+namespace Tyuiu.KozyrevRA.Sprint5.Task1.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\GIGABYTE\source\repos\Tyuiu.KozyrevRA.Sprint5\Tyuiu.KozyrevRA.Sprint5.Task1.V14\bin\Debug\net8.0\OutPutFileTask1.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.KozyrevRA.Sprint5.Task1.V14/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KozyrevRA.Sprint5.Task1.V14.Lib;
+namespace Tyuiu.KozyrevRA.Sprint5.Task1.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Козырев Р. А. | СМАРТБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема:  Класс File. Запись набора данных в текстовый файл                *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Козырев Роман Артёмович | СМАРТБ-25-1                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана функция,(произвести Дана функция,F(x) = табулирование)             *");
+            Console.WriteLine("* f(x) на заданном диапазоне [-5;5] с шагом 1.Произвести проверку деления *");
+            Console.WriteLine("* на ноль.При делении на ноль вернуть значение 0. результат сохранить в   *");
+            Console.WriteLine("* текстовый файл OutPutFileTask1.txt и вывести на консоль в таблицу.      *");
+            Console.WriteLine("* Значения округлить до двух знаков после запятой.                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ФУНКЦИЯ:                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*          Sin(x)                                                         *");
+            Console.WriteLine("*  F(x) = -------- - Cos(x) * 4x - 6                                      *");
+            Console.WriteLine("*          x+1,7                                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("StartValue = " + startValue);
+            Console.WriteLine("StopValue = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(startValue,stopValue);
+            Console.WriteLine("Файл:" + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
